a3b3ab61c3ed238c7891e06c0d7027db51b2afd7
owuordickson <owuordickson@gmail.com>
2019-12-03 14:49:11 +0300

    file path (results)

--- src/init_fuzztx_csv.py
+++ src/init_fuzztx_csv.py
@@ -91,5 +91,5 @@ if __name__ == "__main__":
     wr_text = ("Run-time: " + str(end - start) + " seconds\n")
     wr_text += str(res_text)
     f_name = ('res_x' + str(end).replace('.', '', 1) + '.txt')
-    FuzzTX.write_file(wr_text, '../data/' + f_name)
+    FuzzTX.write_file(wr_text, 'data/' + f_name)
     print(wr_text)
