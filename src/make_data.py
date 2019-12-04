f267e90a7d48cd64fae1b35ac09135e779c41be1
owuordickson <owuordickson@gmail.com>
2019-12-04 13:57:05 +0300

    treating oreme data

--- src/make_data.py
+++ src/make_data.py
@@ -10,14 +10,18 @@ def fetch_site(path, site_id, name):
         new_data = list()
         raw_data = FuzzTX.read_csv(path)
         size = len(raw_data)
+
+        title = []
         for i in range(size):
             line = raw_data[i]
             if i == 0:
-                new_data.append(line)
+                title = line
             else:
                 line_id = line[1]
                 if int(line_id) == site_id:
                     new_data.append(line)
+        new_data.sort(key=lambda x: x[0])
+        new_data.insert(0, title)
         file = (name + '_site' + str(site_id) + '.csv')
         FuzzTX.write_csv(new_data, file)
         # return new_data
@@ -30,8 +34,8 @@ if __name__ == "__main__":
     optparser.add_option('-f', '--inputFile',
                          dest='file',
                          help='path to file containing csv',
-                         default=None,
-                         # default='../data/oreme/GPS.csv',
+                         # default=None,
+                         default='../data/oreme/GPS.csv',
                          type='string')
     optparser.add_option('-n', '--siteId',
                          dest='siteId',
